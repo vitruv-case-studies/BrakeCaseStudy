5d9d963eb9b61ad70a0cf1cca7ead2bb7b02fbed
Benedikt Jutz <benedikt.jutz@kit.edu>
2026-01-21 17:30:08 +0100

    append actual test file

--- Maven_Project/vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/SimulinkAndBrakeSystemTests/BrakeDisk2SimulinkTests.java
+++ Maven_Project/vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/SimulinkAndBrakeSystemTests/BrakeDisk2SimulinkTests.java
@@ -1,5 +1,26 @@
 package tools.vitruv.methodologisttemplate.vsum.SimulinkAndBrakeSystemTests;
 
+import java.util.List;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.junit.jupiter.api.BeforeAll;
+
+
+import tools.vitruv.change.propagation.ChangePropagationSpecification;
+import tools.vitruv.methodologisttemplate.vsum.TestUtil;
+
 public class BrakeDisk2SimulinkTests {
     
+	TestUtil util = new TestUtil();
+	Iterable<ChangePropagationSpecification> necessaryCPS =
+        List.of(
+        );
+
+	@BeforeAll
+	static void setup() {
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("*",
+				new XMIResourceFactoryImpl());
+	}
+
 }
