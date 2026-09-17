005e35a7a7005b53770282b8dd8e713ad3f4c6e5
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2026-09-17 20:29:29 +0200

    Add a further performance test

--- vsum/src/test/java/tools/vitruv/casestudies/brakesystem/vsum/PerformanceTest.java
+++ vsum/src/test/java/tools/vitruv/casestudies/brakesystem/vsum/PerformanceTest.java
@@ -1,5 +1,11 @@
 package tools.vitruv.casestudies.brakesystem.vsum;
 
+import autosar.ARPackage;
+import autosar.AUTOSAR;
+import autosar.ApplicationSwComponentType;
+import autosar.AutoSARFactory;
+import autosar.CompositionSwComponentType;
+import autosar.SwComponentPrototype;
 import brakesystem.ABSSensor;
 import brakesystem.BrakeCaliper;
 import brakesystem.BrakeDisk;
@@ -10,13 +16,16 @@ import brakesystem.BrakesystemFactory;
 import com.google.common.base.Stopwatch;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import mir.reactions.autosar2brakesystem.Autosar2brakesystemChangePropagationSpecification;
+import mir.reactions.autosar2simulink.Autosar2simulinkChangePropagationSpecification;
 import mir.reactions.brakesystem2autosar.Brakesystem2autosarChangePropagationSpecification;
 import mir.reactions.brakesystem2cad.Brakesystem2cadChangePropagationSpecification;
 import mir.reactions.brakesystem2simulink.Brakesystem2simulinkChangePropagationSpecification;
 import mir.reactions.cad2brakesystem.Cad2brakesystemChangePropagationSpecification;
+import mir.reactions.simulink2autosar.Simulink2autosarChangePropagationSpecification;
 import mir.reactions.simulink2brakesystem.Simulink2brakesystemChangePropagationSpecification;
 import org.eclipse.emf.common.util.URI;
 import org.junit.jupiter.api.Test;
@@ -32,15 +41,15 @@ import tools.vitruv.framework.vsum.VirtualModel;
 public class PerformanceTest extends TestBase {
   @Test
   public void testLargeBrakesystemToEach(@TempDir Path tempDir) throws Exception {
-    test(tempDir, List.of(new Brakesystem2autosarChangePropagationSpecification(),
-                          new Autosar2brakesystemChangePropagationSpecification(),
-                          new Brakesystem2cadChangePropagationSpecification(),
-                          new Cad2brakesystemChangePropagationSpecification(),
-                          new Brakesystem2simulinkChangePropagationSpecification(),
-                          new Simulink2brakesystemChangePropagationSpecification()));
+    testLargeBrakesystem(tempDir, List.of(new Brakesystem2autosarChangePropagationSpecification(),
+                                          new Autosar2brakesystemChangePropagationSpecification(),
+                                          new Brakesystem2cadChangePropagationSpecification(),
+                                          new Cad2brakesystemChangePropagationSpecification(),
+                                          new Brakesystem2simulinkChangePropagationSpecification(),
+                                          new Simulink2brakesystemChangePropagationSpecification()));
   }
 
-  private void test(Path tempDir, List<ChangePropagationSpecification> cps) throws IOException {
+  private void testLargeBrakesystem(Path tempDir, List<ChangePropagationSpecification> cps) throws IOException {
     VirtualModel vsum = util.createDefaultVirtualModel(tempDir, cps);
 
     ChangePropagationTracker tracker = new ChangePropagationTracker();
@@ -121,7 +130,71 @@ public class PerformanceTest extends TestBase {
     stopwatch.stop();
     System.out.println("Time: " + stopwatch.elapsed());
 
-    tracker.report();
+    tracker.report(stopwatch.elapsed());
+  }
+
+  @Test
+  public void testSystem(@TempDir Path tempDir) throws Exception {
+    testLargeSystem(tempDir, List.of(new Simulink2autosarChangePropagationSpecification(),
+                                          new Autosar2simulinkChangePropagationSpecification()));
+  }
+
+  private void testLargeSystem(Path tempDir, List<ChangePropagationSpecification> cps) throws IOException {
+    VirtualModel vsum = util.createDefaultVirtualModel(tempDir, cps);
+
+    ChangePropagationTracker tracker = new ChangePropagationTracker();
+    vsum.addChangePropagationListener(tracker);
+
+    Stopwatch stopwatch = Stopwatch.createUnstarted();
+
+    util.modifyView(util
+                        .getDefaultView(vsum, List.of(Brakesystem.class))
+                        .withChangeDerivingTrait(), (CommittableView v) -> {
+      AUTOSAR autosarModel = AutoSARFactory.eINSTANCE.createAUTOSAR();
+      v.registerRoot(autosarModel, URI.createFileURI(tempDir
+                                                        .resolve("example.autosar")
+                                                        .toString()));
+      ARPackage arPackage = AutoSARFactory.eINSTANCE.createARPackage();
+      autosarModel.getArpackage().add(arPackage);
+
+      for (int index = 0; index < 1000; index++) {
+        switch (index % 2) {
+          case 0:
+            ApplicationSwComponentType swComponentType = AutoSARFactory.eINSTANCE.createApplicationSwComponentType();
+            swComponentType.setShortName("AtomicSwComponentType" + index);
+
+            arPackage.getElements().add(swComponentType);
+            break;
+          case 1:
+            CompositionSwComponentType compositionSwComponentType = AutoSARFactory.eINSTANCE.createCompositionSwComponentType();
+            compositionSwComponentType.setShortName("CompositionSwComponentType" + index);
+            ApplicationSwComponentType innerSwComponentType1 = AutoSARFactory.eINSTANCE.createApplicationSwComponentType();
+            innerSwComponentType1.setShortName("InnerSwComponentType1" + index);
+            ApplicationSwComponentType innerSwComponentType2 = AutoSARFactory.eINSTANCE.createApplicationSwComponentType();
+            innerSwComponentType2.setShortName("InnerSwComponentType2" + index);
+
+            arPackage.getElements().add(compositionSwComponentType);
+            arPackage.getElements().add(innerSwComponentType1);
+            arPackage.getElements().add(innerSwComponentType2);
+
+            SwComponentPrototype prototype1 = AutoSARFactory.eINSTANCE.createSwComponentPrototype();
+            prototype1.setType(innerSwComponentType1);
+            compositionSwComponentType.getComponents().add(prototype1);
+
+            SwComponentPrototype prototype2 = AutoSARFactory.eINSTANCE.createSwComponentPrototype();
+            prototype2.setType(innerSwComponentType2);
+            compositionSwComponentType.getComponents().add(prototype2);
+            break;
+        }
+      }
+
+      stopwatch.start();
+    });
+
+    stopwatch.stop();
+    System.out.println("Time: " + stopwatch.elapsed());
+
+    tracker.report(stopwatch.elapsed());
   }
 
   @Override
@@ -142,7 +215,7 @@ public class PerformanceTest extends TestBase {
       changes.add(iterable);
     }
 
-    public void report() {
+    public void report(Duration duration) {
       int numberOfPropagations = 0;
       int numberOfPropagationWaves = 0;
       int numberOfTransactionalChanges = 0;
@@ -168,6 +241,8 @@ public class PerformanceTest extends TestBase {
       System.out.println("Finished propagation, propagations: " + numberOfPropagations + ", waves: "
                              + numberOfPropagationWaves + ", transactional changes: "
                              + numberOfTransactionalChanges + ", e changes: " + numberOfEChanges);
+      System.out.println("propagations per second: " + (numberOfPropagations * 1000.0) / duration.toMillis());
+      System.out.println("e changes per second: " + (numberOfEChanges * 1000.0) / duration.toMillis());
     }
   }
 }
